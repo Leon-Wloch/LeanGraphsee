64f33450df32b6d12246330936207938a85eb681
Leon <leonard.wloch71@gmail.com>
2026-03-10 16:24:53 +0100

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,7 @@
+import Lake
+open Lake DSL
+
+package «lean4-graphsee» where
+  version := v!"0.1.0"
+
+require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.91"
