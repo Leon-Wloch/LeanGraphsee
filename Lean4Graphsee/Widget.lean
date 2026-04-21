f6da8fd3cbb608aa08965599c05bb618d34eedb5
Leon <leonard.wloch71@gmail.com>
2026-04-21 19:59:31 +0200

    Simplify widget logic

--- Lean4Graphsee/Widget.lean
+++ Lean4Graphsee/Widget.lean
@@ -12,21 +12,29 @@ open Lean Meta Server ProofWidgets Jsx
 @[server_rpc_method]
 def KripkeGraph.rpc (props : PanelWidgetProps) : RequestM (RequestTask Html) :=
   RequestM.asTask do
-    let inner : Html ← do
-      if props.goals.isEmpty then
-        return <span>No goals.</span>
-      let some g := props.goals[0]? | unreachable!
-      g.ctx.val.runMetaM {} do
-        let md ← g.mvarId.getDecl
-        let lctx := md.lctx |>.sanitizeNames.run' {options := (← getOptions)}
-        let options ← getOptions
-        if !options.getBool `Kripke.showGraph then
-          return <span></span>
-        Meta.withLCtx lctx md.localInstances do
-          drawKripkeGraph lctx
+    if props.goals.isEmpty then
+      return <span></span>
+
+    let some g := props.goals[0]? | unreachable!
+
+    -- Check whether user set_option to display the graph
+    let showGraph ← g.ctx.val.runMetaM {} do
+      let options ← getOptions
+      return options.getBool `Kripke.showGraph
+
+    if !showGraph then
+      return <span></span>
+
+    -- Generate graphHTML using drawKripkeGraph
+    let graphHTML ← g.ctx.val.runMetaM {} do
+      let md ← g.mvarId.getDecl
+      let lctx := md.lctx |>.sanitizeNames.run' {options := (← getOptions)}
+      Meta.withLCtx lctx md.localInstances do
+        drawKripkeGraph lctx
+
     return <details «open»={true}>
       <summary className="mv2 pointer">Graph Display</summary>
-      <div className="ml1">{inner}</div>
+      <div className="ml1">{graphHTML}</div>
     </details>
 
 @[widget_module]
