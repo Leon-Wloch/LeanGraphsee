d0a4b664074599698d26b17759139496df219c4b
Leon <leonard.wloch71@gmail.com>
2026-04-22 19:28:59 +0200

    Add more options available to users

--- Lean4Graphsee/Options.lean
+++ Lean4Graphsee/Options.lean
@@ -5,10 +5,35 @@ public import Lean.Data.Options
 
 register_option Kripke.showGraph : Bool := {
   defValue := false
-  descr := "Show the graph in the Lean InfoView"
+  descr := "Enable the Graph Display"
 }
 
 register_option Kripke.edgeColours : String := {
-  defValue := "defaultRelationColours"
-  descr := "The colour palette used for edges"
+  defValue := "default"
+  descr := "Colour palette for relation edges (options: default, grayscale, colourblind, vibrant)"
+}
+
+register_option Kripke.edgeLength : Nat := {
+  defValue := 125
+  descr := "Length of edges in pixels"
+}
+
+register_option Kripke.edgeThickness : Nat := {
+  defValue := 2
+  descr := "Thickness of edges in pixels"
+}
+
+register_option Kripke.edgeFontSize : Nat := {
+  defValue := 10
+  descr := "Font size for edge labels in pixels"
+}
+
+register_option Kripke.vertexRadius : Nat := {
+  defValue := 6
+  descr := "Radius of vertex circles in pixels"
+}
+
+register_option Kripke.vertexFontSize : Nat := {
+  defValue := 10
+  descr := "Font size for vertex labels in pixels"
 }
